1fc7b8cae93d065fcdeaa010d35e224d00def261
njrom <njromano16@gmail.com>
2018-07-12 12:18:25 -0400

    fix patient last name not showing up as project name

--- AutomationCode.py
+++ AutomationCode.py
@@ -14,12 +14,10 @@ def main():
     login()
 
     for patient in patients.list:
-        print(patient["name"])
         # --------------------------
         # Workflow for each patient
         # goes here now
         # --------------------------
-
 
 
 def login():
@@ -31,6 +29,33 @@ def login():
     type(DEPARTMENT + Key.ENTER * 3)
     wait(4)
     popup("Please ensure the Epic Theme is set to default \n press ok to proceed")
+
+def createPatient(patient):
+    click("PatientStation.png")
+    wait(1)
+    type(patient["name"] + Key.TAB)
+    type(Key.TAB)
+    type(patient["sex"] + Key.TAB)
+    type(patient["dob"])
+    click("FindPatient.png")
+    wait(2)
+    if exists("GoBack.png"):  # Catches the situation where epic finds similar patient to yours
+        click("GoBack.png")
+        click("New.png")
+        wait(1)
+        if exists("New.png"):
+            click("New.png")
+            wait(1)
+            type("1" + Key.ENTER)
+        else:
+            popup("The patient you are trying to create already exists. \n Change the number in the Patients() call at the top of your script")
+            sys.exit("Program ended due to patient already existing")
+
+    else:  # Normal path for creating patients
+        click("Continue.png")
+        wait(1)
+        click("New.png")
+    wait(3)
 
 
 main()
